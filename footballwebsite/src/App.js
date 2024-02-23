39aba52568a8639c3193dca9e4706aff622847c9
Timothe Goldmann <timothe.goldmann@student.htldonrbirn.at>
2024-02-23 14:43:29 +0100

    Router hinzugefügt

--- footballwebsite/src/App.js
+++ footballwebsite/src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import Button from './components/button';
 import './App.css';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Button />,
+  },
+  {
+    path: "/table",
+    element: <div>back</div>,
+  },
+]);
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    <RouterProvider router={router} />
+
   );
 }
 
