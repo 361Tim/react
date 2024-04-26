e3e41a15a22f1434c52972a1a35a099cc8bc031f
Timothe Goldmann <timothe.goldmann@student.htldonrbirn.at>
2024-04-26 14:50:17 +0200

    Rest Service fertiggestellt

--- Rest-Service/server.js
+++ Rest-Service/server.js
@@ -1,21 +1,36 @@
 const express = require("express")
-const bodyParser = require('body-parser')
 
 const app = express();
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 const port = 3000;
 
-const data = [{ name: "hans", age: 17 }];
+const data = [{ name: "hans", age: 17 },
+{ name: "Sepp", age: 16 },
+{ name: "Peter", age: 18 },
+{ name: "Klaus", age: 19 },];
 
-app.get("/people", (req, res) => {
-    res.send(data);
+app.get("/people/:id", (req, res) => {
+    let id = req.params.id;
+    res.send(data[id]);
 })
 
+app.delete("/people/:id", (req, res) => {
+    let id = req.params.id;
+    data.splice(id, 1);
+    res.send("done")
+})
 app.post("/people", (req, res) => {
-    console.log(req.body);
+    let person = req.body;
+    people.push(person);
+    res.send(person);
+})
 
-    res.send("thanks")
+app.put("/people/:id/name", (req, res) => {
+    let id = req.params.id;
+    let newName = req.body.name;
+    if (data[id]) {
+        data[id].name = newName;
+        res.send(data[id]);
+    }
 })
 app.listen(port, () => {
     console.log("Server running on port " + port);
