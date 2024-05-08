a766438fa16c22b6c4320b0210f330dfefc9928e
Timothe Goldmann <timothe.goldmann@student.htldonrbirn.at>
2024-05-08 12:11:39 +0200

    Rest Service mit db

---
+++ Rest-Service/Untitled-1.sql
@@ -0,0 +1,3 @@
+SELECT * FROM `htld`.`people` LIMIT 1000;
+
+insert into people (firstname, lastname) values ('hans','maier')
--- Rest-Service/server.js
+++ Rest-Service/server.js
@@ -1,27 +1,41 @@
 const express = require("express")
-
+const db = require("./database")
+const bodyParser = require('body-parser')
 const app = express();
-const port = 3000;
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+const port = 3001;
 
 const data = [{ name: "hans", age: 17 },
 { name: "Sepp", age: 16 },
 { name: "Peter", age: 18 },
 { name: "Klaus", age: 19 },];
 
-app.get("/people/:id", (req, res) => {
-    let id = req.params.id;
-    res.send(data[id]);
+app.get("/people", async function (req, res) {
+    try {
+        let result = await db.query("select * from people")
+        res.send(result);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 })
 
-app.delete("/people/:id", (req, res) => {
+app.delete("/people", async (req, res) => {
     let id = req.params.id;
     data.splice(id, 1);
     res.send("done")
 })
-app.post("/people", (req, res) => {
+app.post("/people", async function (req, res) {
+
     let person = req.body;
-    people.push(person);
-    res.send(person);
+    console.log(person);
+    let sql = "insert into people (firstname, lastname) values(?,?)";
+    try {
+        let result = await db.query(sql, [person.firstname, person.lastname])
+        res.send(result);
+    } catch (error) {
+        res.status(404).send(error.message);
+    }
 })
 
 app.put("/people/:id/name", (req, res) => {
